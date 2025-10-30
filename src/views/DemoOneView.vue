06a0bc1d9dc5b5d7495c9e9882e771681f698f57
rorikoron <sub.ac.000000001@gmail.com>
2025-10-30 10:55:22 +0800

    feat: now can receive demo health connect data from application

--- src/views/DemoOneView.vue
+++ src/views/DemoOneView.vue
@@ -1,7 +1,20 @@
 <script setup lang="ts">
+import { onMounted, ref } from 'vue';
 import demoOneJson from '../../public/mock/demo_one.json';
+import { useConnectionMessage } from '@/composables/useConnectionMessage';
+import { useHandleConnectionData } from '@/composables/useHandleConnectionData';
 
 const demoOneData = demoOneJson.data;
+
+const demoHealthConnectData = ref<string>('');
+
+const handleHealthConnectData = (event: { data: string }) => {
+  console.log(event.data);
+};
+useHandleConnectionData(handleHealthConnectData);
+onMounted(() => {
+  useConnectionMessage('health_connect', null);
+});
 </script>
 <template>
   <h1 class="text-bold">Microservice Demo 1</h1>
@@ -9,4 +22,7 @@ const demoOneData = demoOneJson.data;
   <h3 v-for="item in demoOneData.demo_items" :key="item.id">
     {{ item.id }} - {{ item.title }}: {{ item.description }}
   </h3>
+
+  <span v-if="demoHealthConnectData.length">Health Connect Data: {{ demoHealthConnectData }}</span>
+  <span v-else>Fetched no data</span>
 </template>
